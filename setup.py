1946a20281aa4706e294882fadd63e91195c2bfc
ThebeardedOn3 <jbeard6@mix.wvu.edu>
2023-01-17 21:03:33 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -4,14 +4,13 @@ import os
 
 current = os.path.dirname(os.path.realpath(__file__))
 
-# setup(
-#     name="experimentlab",
-#     version="0.0.1",
-#     install_requires=["nestifydict",
-#                       "reconfigurator"],
-# )
+setup(
+    name="experimentlab",
+    version="0.0.1",
+    install_requires=["nestifydict",
+                      "reconfigurator"],
+)
 
-# import reconfigurator as rc
 import nestifydict as nd
 import json
 
@@ -19,7 +18,6 @@ with open(current + "/experimentlab/config/core/core_default.json", "rb") as f:
     core_default = json.load(f)
     
 defaults = { "path": current + "/experimentlab/config/"}
-
 defaults = nd.merge(core_default, defaults)
 
 with open(current + "/experimentlab/config/core/config.json", "w+") as f:
